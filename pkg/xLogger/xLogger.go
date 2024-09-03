e32415ce23e7a2208b65eaee2dba6903ab0c92ea
Aleksei Anisimov <i@1xtr.dev>
2024-09-03 07:52:23 +0300

    feat(xLogger): added `GetLogger` method; added logging to file if env IS_DEV = true

--- pkg/xLogger/xLogger.go
+++ pkg/xLogger/xLogger.go
@@ -1,12 +1,15 @@
 package xLogger
 
 import (
+	"fmt"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 )
+
+const logDir = "logs"
 
 var Logger zerolog.Logger
 
@@ -34,7 +37,7 @@ func init() {
 		}
 	}
 
-	// UNIX Time is faster and smaller than most timestamps, but logs systems wait RFC3339
+	// UNIX Time is faster and smaller than most timestamps
 	zerolog.TimeFieldFormat = time.RFC3339Nano
 	// Log time always in UTC
 	zerolog.TimestampFunc = func() time.Time {
@@ -43,11 +46,45 @@ func init() {
 	}
 	// Default level for this example is info, unless debug flag is present
 	zerolog.SetGlobalLevel(logLevel)
-	Logger = zerolog.New(os.Stdout).With().Caller().Timestamp().Logger()
+	isDev := os.Getenv("IS_DEV")
+	if isDev == "true" {
+		checkLogFolder(logDir)
+		consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
+		fileName := fmt.Sprintf("./%s/%v.log", logDir, time.Now().Unix())
+		runLogFile, _ := os.OpenFile(
+			fileName,
+			os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+			0664,
+		)
+		multi := zerolog.MultiLevelWriter(consoleWriter, runLogFile)
+		Logger = zerolog.New(multi).With().Caller().Timestamp().Logger()
+	} else {
+		Logger = zerolog.New(os.Stdout).With().Caller().Timestamp().Logger()
+	}
 }
 
 // AddRequestIdToLogger Set requestID in meta for every request
 func AddRequestIdToLogger(requestID string) {
 	Logger = zerolog.New(os.Stdout).With().
 		Caller().Timestamp().Str("requestID", requestID).Logger()
+}
+
+func GetLogger(componentName string) zerolog.Logger {
+	return Logger.With().Str("component", componentName).Logger()
+}
+
+// checkLogFolder creating logs dir if not exist
+func checkLogFolder(folder string) {
+	// Check if the directory exists
+	if _, err := os.Stat(folder); os.IsNotExist(err) {
+		// Directory does not exist, create it
+		err := os.Mkdir(folder, 0755)
+		if err != nil {
+			fmt.Println("Error creating directory:", err)
+			return
+		}
+		fmt.Println("Directory created:", folder)
+	} else {
+		fmt.Println("Directory already exists:", folder)
+	}
 }
